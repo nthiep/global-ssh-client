d2fb180f08c3d3b2a5a60295e37d9dd09dceabcd
nthiep <hiep.iuh@gmail.com>
2015-04-17 00:07:08 +0700

    edit windows plf

--- gsh/config.py
+++ gsh/config.py
@@ -11,15 +11,25 @@ windows = True if platform.system() == "Windows" else False
 # change config file if you move to another place
 root 		= os.path.abspath(os.sep)
 if windows:
+	GSH_DIR 	= os.path.join(root, "gsh")
 	CONFIG_FILE = os.path.join(root, "gsh", "gsh.conf")
-	LOG_FILENAME= os.path.join(root, "log", "gshs.log")
+	LOG_FILENAME= os.path.join(root, "gsh", "gsh.log")
 	PLATFORM 	= platform.system() + platform.release()
 else:
+	GSH_DIR 	= os.path.join(root, "etc", "gsh")
 	CONFIG_FILE = os.path.join(root, "etc", "gsh", "gsh.conf")
-	LOG_FILENAME= os.path.join(root, "var", "log", "gshs.log")
+	LOG_FILENAME= os.path.join(root, "var", "log", "gsh.log")
 	info		= platform.linux_distribution()
 	PLATFORM 	= " ".join(info)
 parser = ConfigParser()
+if not os.path.exists(CONFIG_FILE):
+	parser.add_section('config')
+	parser.set('config','ssh_server', '127.0.0.1')
+	parser.set('config','ssh_port', 22)
+	parser.set('config','debug', True)
+	f = open(CONFIG_FILE, "w")
+	parser.write(f)
+	f.close()
 parser.read(CONFIG_FILE)
 # enable or disable debug model
 DEBUG 	= False
@@ -39,7 +49,6 @@ _node 		= _node.replace(" ", "0")
 _mac 		= ':'.join([''.join(i) for i in map(None, *(_node[::2], _node[1::2]))])
 
 MAC_ADDR 	= _mac
-GSH_DIR 	= parser.get('config', 'gsh_dir')
 NET_DIR		= os.path.join(GSH_DIR, "net")
 KEY_FILE 	= os.path.join(GSH_DIR, "gsh.key")
 
